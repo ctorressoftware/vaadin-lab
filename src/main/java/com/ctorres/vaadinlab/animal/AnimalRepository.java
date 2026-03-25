e3165ac2d1f8bb3b57603bee1bfb647b0463d559
Carlos Torres <ctorressoftware@hotmail.com>
2026-03-25 00:17:08 -0400

    chore: clean git tracking based on .gitignore

--- src/main/java/com/ctorres/vaadinlab/animal/AnimalRepository.java
+++ src/main/java/com/ctorres/vaadinlab/animal/AnimalRepository.java
@@ -1,4 +1,70 @@
 package com.ctorres.vaadinlab.animal;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@Repository
 public class AnimalRepository {
+
+    private final Set<Animal> animalsForAdoption = new HashSet<>(0);
+
+    public AnimalRepository() {
+        animalsForAdoption.add(
+                new Animal(
+                        "Rufus",
+                        Gender.MALE,
+                        1,
+                        Specie.DOG,
+                        "Good buddy")
+        );
+    }
+
+    public Animal save(Animal animal) {
+
+        if (!isValid(animal)) return null;
+
+        boolean saved = animalsForAdoption.add(animal);
+
+        if (saved) {
+            return animal;
+        } else {
+            throw new RuntimeException("An error occurred trying to save: " + animal);
+        }
+    }
+
+    public Set<Animal> findAll() {
+        return animalsForAdoption;
+    }
+
+    private boolean isValid(Animal animal) {
+
+        if (animal == null) {
+            throw new RuntimeException("Animal is required");
+        }
+
+        if (animal.getName() == null || animal.getName().isBlank()) {
+            throw new RuntimeException("name property is required");
+        }
+
+        if (!Arrays.stream(Gender.values()).toList().contains(animal.getGender())) {
+            throw new RuntimeException("gender property is required");
+        }
+
+        if (animal.getAge() < 0) {
+            throw new RuntimeException("age property is required");
+        }
+
+        if (!Arrays.stream(Specie.values()).toList().contains(animal.getSpecie())) {
+            throw new RuntimeException("specie property is required");
+        }
+
+        if (animal.getPersonality().isBlank()) {
+            throw new RuntimeException("personality property is required");
+        }
+
+        return true;
+    }
 }
--- src/main/java/com/ctorres/vaadinlab/animal/ui/AnimalScreen.java
+++ src/main/java/com/ctorres/vaadinlab/animal/ui/AnimalScreen.java
@@ -1,4 +1,58 @@
 package com.ctorres.vaadinlab.animal.ui;
 
-public class AnimalScreen {
+import com.ctorres.vaadinlab.animal.Animal;
+import com.ctorres.vaadinlab.animal.AnimalRepository;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.router.Route;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Route("/animals")
+public class AnimalScreen extends Div {
+
+    private final AnimalRepository animalRepository;
+
+    public AnimalScreen(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
+        draw();
+    }
+
+    private void title(String text) {
+        add(new H2(text));
+    }
+
+    private void showAnimals(Set<Animal> animals) {
+
+        if (animals == null) throw new RuntimeException("animals is required");
+        if (animals.isEmpty()) add("There are no animals for adoption :(");
+
+        var table = new NativeTable();
+        table.add(new NativeTableHeader(
+                new NativeTableRow(
+                        new NativeTableHeaderCell("name"),
+                        new NativeTableHeaderCell("gender"),
+                        new NativeTableHeaderCell("age"),
+                        new NativeTableHeaderCell("specie"),
+                        new NativeTableHeaderCell("personality")
+                )
+        ));
+
+        table.addBody().add(animals.stream().map(animal ->
+                new NativeTableRow(
+                        new NativeTableCell(animal.getName()),
+                        new NativeTableCell(animal.getGender().name()),
+                        new NativeTableCell(String.valueOf(animal.getAge())),
+                        new NativeTableCell(animal.getSpecie().name()),
+                        new NativeTableCell(animal.getPersonality())
+                )
+        ).collect(Collectors.toList()));
+        add(table);
+    }
+
+    private void draw() {
+        title("Animals for adoption");
+        showAnimals(animalRepository.findAll());
+    }
+
 }
