6ea8f8ed09cee1c7d1b04417930be745a630ddd7
Carlos Torres <ctorressoftware@hotmail.com>
2026-03-29 17:22:10 -0400

    feat: adjust dialog width to show the data with new column properly

--- src/main/java/com/ctorres/vaadinlab/animal/ui/AnimalDialog.java
+++ src/main/java/com/ctorres/vaadinlab/animal/ui/AnimalDialog.java
@@ -27,7 +27,7 @@ public class AnimalDialog extends Dialog {
 
     private void configureModalDialog() {
         setHeaderTitle("Add animal");
-        setWidth(20f, Unit.PERCENTAGE);
+        setWidth(40f, Unit.PERCENTAGE);
         add(form);
     }
 
