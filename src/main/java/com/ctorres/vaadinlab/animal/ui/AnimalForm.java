12a290df206d8c361d0609548d27c130d522405e
Carlos Torres <ctorressoftware@hotmail.com>
2026-03-29 17:23:05 -0400

    feat: add imageUrl textfield to the form and configure it in each point of the file

--- src/main/java/com/ctorres/vaadinlab/animal/ui/AnimalForm.java
+++ src/main/java/com/ctorres/vaadinlab/animal/ui/AnimalForm.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 public class AnimalForm extends FormLayout {
     private final TextField nameField = new TextField("Name");
+    private final TextField imageUrlField = new TextField("ImageUrl");
     private final ComboBox<Gender> genderComboBox = new ComboBox<>("Gender");
     private final IntegerField ageField = new IntegerField("Age");
     private final ComboBox<Specie> specieComboBox = new ComboBox<>("Specie");
@@ -32,6 +33,7 @@ public class AnimalForm extends FormLayout {
 
     private void configureFields() {
         nameField.setWidthFull();
+        imageUrlField.setWidthFull();
         ageField.setWidthFull();
         personalityField.setWidthFull();
     }
@@ -46,22 +48,24 @@ public class AnimalForm extends FormLayout {
     }
 
     private void addFields() {
-        add(nameField, genderComboBox, ageField, specieComboBox, personalityField);
+        add(nameField, imageUrlField, genderComboBox, ageField, specieComboBox, personalityField);
     }
 
     public Optional<Animal> getFormDataObject() {
         final String name = nameField.getValue();
+        final String image = imageUrlField.getValue();
         final Gender gender = genderComboBox.getValue();
         final Integer age = ageField.getValue();
         final Specie specie = specieComboBox.getValue();
         final String personality = personalityField.getValue();
 
-        if (name == null || gender == null || age == null || specie == null || personality == null) {
+        if (name == null || image == null || gender == null || age == null || specie == null || personality == null) {
             return Optional.empty();
         }
 
         return Optional.of(new Animal(
                 name,
+                image,
                 gender,
                 age,
                 specie,
