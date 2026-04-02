0f30759ee6ae3e7c414b4a59ccf97342bc8bc8bd
Carlos Torres <ctorressoftware@hotmail.com>
2026-04-02 19:49:49 -0400

    feat: add existsAnimalById method to check if animal exists before save an edited one

--- src/main/java/com/ctorres/vaadinlab/animal/AnimalRepository.java
+++ src/main/java/com/ctorres/vaadinlab/animal/AnimalRepository.java
@@ -4,10 +4,15 @@ import com.ctorres.vaadinlab.entity.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, UUID> {
     Optional<Animal> findAnimalByName(String name);
+
+    List<Animal> findAnimalById(UUID id);
+
+    boolean existsAnimalById(UUID id);
 }
