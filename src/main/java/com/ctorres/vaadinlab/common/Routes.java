c77e4e93711d19f99e417f60d80f9d3d67dcdd13
Carlos Torres <ctorressoftware@hotmail.com>
2026-04-04 00:29:32 -0400

    refactor: add new route and edit default one adding 'PAGE' as a suffix

--- src/main/java/com/ctorres/vaadinlab/common/Routes.java
+++ src/main/java/com/ctorres/vaadinlab/common/Routes.java
@@ -1,5 +1,6 @@
 package com.ctorres.vaadinlab.common;
 
 public class Routes {
-    public static final String DEFAULT_ROUTE = "/";
+    public static final String DEFAULT_PAGE = "/";
+    public static final String DETAILS_PAGE = "/details";
 }
